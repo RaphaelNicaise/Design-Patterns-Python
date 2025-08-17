ef8a4b1fabd6bf97ff05312dcfcd0c2e13c476f7
RaphaelNicaise <raphanicaise@gmail.com>
2025-08-17 10:50:06 -0300

    Prototype

---
+++ Creational/Prototype/prototype.py
@@ -0,0 +1,28 @@
+import copy
+
+class Prototype:
+    def clone(self):
+        return copy.deepcopy(self) # metodo que retorna una copia completa del objeto completo
+    
+class ConcretePrototype(Prototype):
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+    
+    def __str__(self):
+        return f"{self.name}: {self.value}"
+    
+# creamos prototipo, clase que nos va a ayudar a crear clases despues
+prototype = ConcretePrototype("prot", 1)
+print(prototype) # -> prot: 1
+
+clone = prototype.clone() # creamos objeto clon en base al prototipo
+print(clone) # -> prot: 1
+
+# modificamos al clone
+clone.value = 2
+
+# verificamos que sean distinto
+print("\nVerificacion:")
+print(f"Prototype -> {prototype}")
+print(f"Clone -> {clone}")
